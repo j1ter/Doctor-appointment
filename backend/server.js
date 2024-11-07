2169a91b3ae6b836606b341342e7e97d76c839f1
Daniyal Samat <daniyal.samat@narxoz.kz>
2024-11-07 15:25:00 +0500

    Create API to add Doctor data in Database

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,23 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './routes/adminRoute.js';
 
+
 // app config
-const app = express();
-const port = process.env.PORT || 4000;
-connectDB();
-connectCloudinary();
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 // middlewares
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 // api endpoints
-app.use('/api/admin', adminRouter);
-// localhost:4000/api/admin/add-doctor
+app.use('/api/admin', adminRouter)
+// localhost
 
 app.get('/', (req, res) => {
-    res.send('API WORKING');
+    res.send('API WORKING')
 });
 
-app.listen(port, () => console.log("Server Started", port));
+app.listen(port, () => console.log("Server Started", port))
