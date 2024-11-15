ca9c0805b54296b21d4ae0fe4a21b91fe13ab321
DepthHunter <nurassyl.makhambetali@narxoz.kz>
2024-11-15 11:34:46 +0500

    alikhan delai

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -16,4 +16,18 @@ const changeAvailabity = async (req, res) => {
     }
 };
 
-export {changeAvailabity};
+
+const doctorList = async (req,res) => {
+    try {
+        
+        const doctors = await doctorModel.find({}).select(['-password','-email'])
+
+        res.json({success:true,doctors})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
+export {changeAvailabity, doctorList};
