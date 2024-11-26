c5e9bdb319e438abc4dbf6cabac28e1509efd41c
Alikhan-Batyr <adaetskz@gmail.com>
2024-11-26 13:04:46 +0600

    Create Dashboard Page for admin panel

--- Admin/src/context/AdminContext.jsx
+++ Admin/src/context/AdminContext.jsx
@@ -9,7 +9,7 @@ const AdminContextProvider = (props) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors, setDoctors] = useState([]);
     const [appointments, setAppointments] = useState([])
-
+    const [dashData, setDashData] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -58,7 +58,7 @@ const AdminContextProvider = (props) => {
                 
 
         } catch (error) {
-            toast.error(data.message)
+            toast.error(error.message)
         }
     }
 
@@ -76,7 +76,25 @@ const AdminContextProvider = (props) => {
 
             
         } catch (error) {
-            toast.error(data.message)
+            toast.error(error.message)
+        }
+    }
+
+
+    const getDashData = async () => {
+        try {
+
+            const {data} = await axios.get(backendUrl + '/api/admin/dashboard', {headers: {aToken}})
+
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
         }
     }
 
@@ -90,7 +108,8 @@ const AdminContextProvider = (props) => {
         changeAvailabity,
         appointments, setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData, getDashData
     };
 
     return (
