83ec95c6d3bb77c1b74f92518028f0f8f46d9fc3
Daniyal Samat <daniyal.samat@narxoz.kz>
2024-11-29 19:44:21 +0500

    Display and manage appointments in Doctor panel

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,6 +1,7 @@
 import doctorModel from "../models/doctorModel.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import appointmentModel from '../models/appointmentModel.js';
 
 const changeAvailabity = async (req, res) => {
     try {
@@ -58,4 +59,64 @@ const loginDoctor = async (req, res) => {
     }
 }
 
-export {changeAvailabity, doctorList, loginDoctor};
+// API to get doctor appointments for doctor panel
+const appointmentsDoctor = async (req, res) => {
+    try {
+        const { docId } = req.body;
+        const appointments = await appointmentModel.find({docId});
+
+        res.json({success: true, appointments});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// API to mark appointment completed for doctor panel
+const appointmentComplete = async (req, res) => {
+    try {
+        const {docId, appointmentId} = req.body;
+        
+        const appointmentData = await appointmentModel.findById(appointmentId);
+
+        if (appointmentData && appointmentData.docId === docId) {
+            await appointmentModel.findByIdAndUpdate(appointmentId, {isCompleted: true});
+
+            return res.json({success: true, message: 'Appointment Completed'});
+        } else {
+            return res.json({success: false, message: 'Mark Failed'});
+        }
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// API to cancel appointment for doctor panel
+const appointmentCancel = async (req, res) => {
+    try {
+        const {docId, appointmentId} = req.body;
+        
+        const appointmentData = await appointmentModel.findById(appointmentId);
+
+        if (appointmentData && appointmentData.docId === docId) {
+            await appointmentModel.findByIdAndUpdate(appointmentId, {cancelled: true});
+
+            return res.json({success: true, message: 'Appointment Cancelled'});
+        } else {
+            return res.json({success: false, message: 'Cancellation Failed'});
+        }
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
+export {changeAvailabity, doctorList, loginDoctor, appointmentsDoctor, appointmentCancel, appointmentComplete};
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 function MyAppointments() {
 
