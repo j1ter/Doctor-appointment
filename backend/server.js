d6821a367829958860f8395534d0597d8d3e01b5
Daniyal Samat <daniyal.samat@narxoz.kz>
2025-05-07 22:16:24 +0500

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,18 @@ connectDB()
 connectCloudinary()
 
 // middlewares
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
+app.use(
+    cors({
+        origin: 'http://localhost:5173', // Убрали завершающий слеш
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+);
+app.options('*', cors()); // Поддержка preflight-запросов
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // api endpoints
 app.use('/api/admin', adminRouter)
